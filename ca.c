d232d96c3836191d526fc429967ff5cb309b855a
Youssef Mohamed Mohamed <58553094+youssef1234-code@users.noreply.github.com>
2025-03-07 15:05:39 +0200

    fix file name to match milestone description and include missing header file
    fix file name to match milestone description and include missing header file

--- ca.c
+++ ca.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-
+// #include <stdlib.h>
 int main() {
-    char *filename= malloc(256);  // allocating 256 bytes to the file name
+    // char *filename= malloc(256);  // allocating 256 bytes to the file name
 
-    if (filename == NULL) {   //checking if memory was allocated successfully
-        printf("Memory allocation failed.\n");
-        return 1;       //convention in C to represent an error
-    }
+    // if (filename == NULL) {   //checking if memory was allocated successfully
+    //     printf("Memory allocation failed.\n");
+    //     return 1;       //convention in C to represent an error
+    // }
 
-    // Ask the user for the file's name
-    printf("Please enter the file name: ");
-    scanf("%255s", filename);
+    // // Ask the user for the file's name
+    // printf("Please enter the file name: ");
+    // scanf("%255s", filename);
 
     // Open the file in read mode ("r")
+    char filename[] = "test.txt";
     FILE *file = fopen(filename, "r");
-    free(filename);
+    // free(filename);
     if (file == NULL) {  // displays error if cannot open file
         printf("Error in opening file: %s", filename);
         return 1;       //convention in C to represent an error
