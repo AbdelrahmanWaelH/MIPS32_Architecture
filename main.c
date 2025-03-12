d9c764ef11109e6922898ddea3ea0fb9040d48f4
AbdelrahmanWaelH <abdelrahmanw20@gmail.com>
2025-03-12 22:26:48 +0200

    removed comments cuz yknow

--- main.c
+++ main.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
-// #include <stdlib.h>
 int main() {
-    // char *filename= malloc(256);  // allocating 256 bytes to the file name
-
-    // if (filename == NULL) {   //checking if memory was allocated successfully
-    //     printf("Memory allocation failed.\n");
-    //     return 1;       //convention in C to represent an error
-    // }
-
-    // // Ask the user for the file's name
-    // printf("Please enter the file name: ");
-    // scanf("%255s", filename);
-
-    // Open the file in read mode ("r")
-    char filename[] = "test.txt";
-    FILE *file = fopen(filename, "r");
-    // free(filename);
-    if (file == NULL) {  // displays error if cannot open file
-        printf("Error in opening file: %s", filename);
-        return 1;       //convention in C to represent an error
-    }
-
-    // Read each character in each line, and displays it one at a time
-    char c;
-    while ((c=fgetc(file)) != EOF) {
-        printf("%c", c);
-    }
-
-    // Close the file
-    fclose(file);
-
-    return 0;       //convention in C to represent successful execution   
+	char filename[] = "test.txt";
+	FILE *file = fopen(filename, "r");
+	if (file == NULL) {
+		printf("Error in opening file: %s", filename);
+		return 1;
+	}
+	char c;
+	while ((c = fgetc(file)) != EOF) {
+		printf("%c", c);
+	}
+	fclose(file);
+	return 0;
 }
